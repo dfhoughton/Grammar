6a81766bb4c1b94e756b453adec24d462179e3cc
David Houghton <dhoughton@complyoninc.com>
2011-04-08 14:22:55 -0400

    fixed dynamic list bug in javascript

--- documentation/html/scripts/footnotes.js
+++ documentation/html/scripts/footnotes.js
@@ -12,10 +12,16 @@
  * black; ... } for the section where the footnote text will appear
  * 
  * 2) In the body of your document wherever you want a footnote you enclose the
- * text like so: lorem ipsum<a class="fn">sic transit gloria mundi</a> etc.
+ * text like so: lorem ipsum<span class="fn">sic transit gloria mundi</span>
+ * etc.
  * 
- * 3) At the bottom of the page where you want to footnotes to appear you
- * include the following: <div id="footnotes"><script>footnotes()</script></div>
+ * 3) You trigger footnotes() on page load like so: <body
+ * onLoad="footnotes()">...
+ * 
+ * 4) If you want to control the placement of your footnotes, where you want to
+ * footnotes to appear you include the following: <div id="footnotes"></div>.
+ * Otherwise, the footnotes DIV will be appended to the list of child nodes of
+ * the body element, so they will appear last.
  * 
  * Provided you have linked this script to the page it will modify the page
  * contents, replacing your invisible footnote anchor elements with links to
@@ -29,38 +35,47 @@
 
 function footnotes() {
 	var fnSection = document.getElementById("footnotes");
-	if (fnSection) {
-		var fns = document.getElementsByClassName("fn");
-		var len = fns.length;
-		var found = false;
-		var index = 1;
-		for ( var i = 0; i < len; i++) {
-			var a = fns[i];
-			if (a.nodeName == 'SPAN') {
-				found = true;
-				a.removeAttribute('class');
-				var bname = 'fn' + index + '_bottom';
-				var tname = 'fn' + index + '_top';
-				var topA = document.createElement("a");
-				topA.setAttribute('href', '#' + bname);
-				topA.setAttribute('name', tname);
-				topA.setAttribute('class', 'footnote');
-				topA.innerHTML = index;
-				a.parentNode.replaceChild(topA, a);
-				var textDiv = document.createElement("div");
-				var bottomA = document.createElement("a");
-				bottomA.setAttribute('href', '#' + tname);
-				bottomA.setAttribute('name', bname);
-				bottomA.setAttribute('class', 'footnote');
-				bottomA.innerHTML = index;
-				textDiv.appendChild(bottomA);
-				textDiv.appendChild(document.createTextNode(' '));
-				textDiv.appendChild(a);
-				fnSection.appendChild(textDiv);
-				index++;
-			}
+	var fnsExists = fnSection && 1;
+	if (!fnsExists) {
+		fnSection = document.createElement('div');
+		fnSection.setAttribute('id', 'footnotes');
+	}
+	var fns = document.getElementsByClassName("fn");
+	// copy items to new array that won't be dynamically altered
+	var ar = new Array();
+	var len = fns.length;
+	for ( var i = 0; i < len; i++) {
+		var span = fns[i];
+		ar[i] = span;
+	}
+	var found = false;
+	var index = 1;
+	for ( var i = 0; i < len; i++) {
+		var span = ar[i];
+		if (span.nodeName == 'SPAN') {
+			found = true;
+			span.removeAttribute('class');
+			var bname = 'fn' + index + '_bottom';
+			var tname = 'fn' + index + '_top';
+			var topA = document.createElement("a");
+			topA.setAttribute('href', '#' + bname);
+			topA.setAttribute('name', tname);
+			topA.setAttribute('class', 'footnote');
+			topA.innerHTML = index;
+			span.parentNode.replaceChild(topA, span);
+			var textDiv = document.createElement("div");
+			var bottomA = document.createElement("a");
+			bottomA.setAttribute('href', '#' + tname);
+			bottomA.setAttribute('name', bname);
+			bottomA.setAttribute('class', 'footnote');
+			bottomA.innerHTML = index;
+			textDiv.appendChild(bottomA);
+			textDiv.appendChild(document.createTextNode(' '));
+			textDiv.appendChild(span);
+			fnSection.appendChild(textDiv);
+			index++;
 		}
-		if (!found)
-			fnSection.parentNode.removeChild(fnSection);
+		if (found && !fnsExists)
+			document.body.appendChild(fnSection);
 	}
 }
