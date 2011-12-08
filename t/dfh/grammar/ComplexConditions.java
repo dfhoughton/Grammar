0745aa810ec0fa7e4da3ec143b314719c58460d0
David Houghton <dhoughton@complyoninc.com>
2011-12-08 11:55:29 -0500

    initial unit test for complex conditions

---
+++ t/dfh/grammar/ComplexConditions.java
@@ -0,0 +1,61 @@
+package dfh.grammar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests the parsing and application of complex conditions such as
+ * (greater_than_10 | less_than_0).
+ * <p>
+ * <b>Creation date:</b> Dec 8, 2011
+ * 
+ * @author David Houghton
+ * 
+ */
+@SuppressWarnings("serial")
+public class ComplexConditions {
+	private static final IntegerCondition lt100 = new IntegerCondition() {
+		@Override
+		public boolean passes(int i) {
+			return i < 100;
+		}
+	};
+	private static final IntegerCondition gt10 = new IntegerCondition() {
+		@Override
+		public boolean passes(int i) {
+			return i > 10;
+		}
+	};
+	private static final IntegerCondition eq5 = new IntegerCondition() {
+		@Override
+		public boolean passes(int i) {
+			return i == 5;
+		}
+	};
+
+	@Test
+	public void simpleConjunction() {
+		String[] rules = {
+		//
+		"ROOT = /\\d++/ (lt100 gt10)",//
+		};
+		Grammar g = new Grammar(rules);
+		g.defineCondition("lt100", lt100);
+		g.defineCondition("gt10", gt10);
+		assertNotNull("simple conjunction", g.matches("12").match());
+	}
+
+	@Test
+	public void simpleDisjunction() {
+		String[] rules = {
+		//
+		"ROOT = /\\d++/ (gt10 | eq5)",//
+		};
+		Grammar g = new Grammar(rules);
+		g.defineCondition("eq5", eq5);
+		g.defineCondition("gt10", gt10);
+		assertNotNull("simple disjunction", g.matches("5").match());
+	}
+
+}
