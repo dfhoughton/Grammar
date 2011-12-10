522154cbe2116c66a2004bf59d3643a5db100061
David Houghton <dhoughton@complyoninc.com>
2011-12-10 11:20:43 -0500

    added class representing uplevel back reference

---
+++ src/dfh/grammar/UplevelBackReferenceFragment.java
@@ -0,0 +1,30 @@
+package dfh.grammar;
+
+/**
+ * Like {@link BackReferenceFragment} but allows repetition and creates an
+ * {@link UpLevelBackReferenceRule}.
+ * <p>
+ * <b>Creation date:</b> Dec 10, 2011
+ * 
+ * @author David Houghton
+ * 
+ */
+public class UplevelBackReferenceFragment extends RepeatableRuleFragment {
+
+	protected int reference;
+
+	/**
+	 * Generates a fragment from the given reference number.
+	 * 
+	 * @param reference
+	 */
+	public UplevelBackReferenceFragment(int reference) {
+		this.reference = reference - 1;
+	}
+
+	@Override
+	public String toString() {
+		return Integer.toString(reference + 1) + '^';
+	}
+
+}
