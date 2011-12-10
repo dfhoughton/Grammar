3086c1b584d5e0d27f02eb56ada8d17611552bd0
David Houghton <dhoughton@complyoninc.com>
2011-12-10 11:18:46 -0500

    added more coverage to unit test for back reference booboos

--- t/dfh/grammar/BackReferenceTest.java
+++ t/dfh/grammar/BackReferenceTest.java
@@ -3,6 +3,7 @@ package dfh.grammar;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 
@@ -62,4 +63,90 @@ public class BackReferenceTest {
 		Match n = m.match();
 		assertNull("didn't find ned", n);
 	}
+
+	@Test
+	public void tooSmallTest() throws GrammarException, IOException {
+		String[] rules = {
+		//
+		"<ROOT> = /[ab]/ 'foo' 0",//
+		};
+		try {
+			new Grammar(rules);
+			fail("should have throw exception");
+		} catch (GrammarException e) {
+			assertTrue(e.getMessage().equals(
+					"back references must be greater than 0"));
+		}
+	}
+
+	@Test
+	public void tooBigTest() throws GrammarException, IOException {
+		String[] rules = {
+		//
+		"<ROOT> = /[ab]/ 'foo' 3",//
+		};
+		try {
+			new Grammar(rules);
+			fail("should have throw exception");
+		} catch (GrammarException e) {
+			assertTrue(e.getMessage().equals("back reference 3 is too big"));
+		}
+	}
+
+	@Test
+	public void groupTest() throws GrammarException, IOException {
+		String[] rules = {
+		//
+		"<ROOT> = [ /[ab]/ 'foo' 1 ]{2,}",//
+		};
+		Grammar g = new Grammar(rules);
+		int count = 0;
+		Matcher m = g.find("afooabfoob afooa foobfoob");
+		while (m.match() != null)
+			count++;
+		assertTrue("back reference in group worked", count == 1);
+	}
+
+	@Test
+	public void groupTooSmallTest() throws GrammarException, IOException {
+		String[] rules = {
+		//
+		"<ROOT> = [ /[ab]/ 'foo' 0 ]{2,}",//
+		};
+		try {
+			new Grammar(rules);
+			fail("should have throw exception");
+		} catch (GrammarException e) {
+		}
+	}
+
+	@Test
+	public void groupTooBigTest() throws GrammarException, IOException {
+		String[] rules = {
+		//
+		"<ROOT> = [ /[ab]/ 'foo' 4 ]{2,}",//
+		};
+		try {
+			new Grammar(rules);
+			fail("should have throw exception");
+		} catch (GrammarException e) {
+		}
+	}
+
+	@Test
+	public void repetitionTest() throws GrammarException, IOException {
+		String[] rules = {
+		//
+		"<ROOT> = /[ab]/ 'foo' 1{2,}",//
+		};
+		try {
+			new Grammar(rules);
+			fail("should have throw exception");
+		} catch (GrammarException e) {
+			assertTrue(e
+					.getMessage()
+					.equals("simple back reference cannot be modified with repetition suffix; use uplevel backreference; e.g., 1^"));
+		}
+	}
+
 }
