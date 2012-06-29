435a27af674728bc6b5ad4d030ad2c4db8a4c744
David Houghton <dhoughton@complyoninc.com>
2012-06-29 04:48:26 -0400

    tinkering with whitespace delimiter test

--- t/dfh/grammar/WhitespaceDelimiterTest.java
+++ t/dfh/grammar/WhitespaceDelimiterTest.java
@@ -2,6 +2,11 @@ package dfh.grammar;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 import org.junit.Test;
 
 public class WhitespaceDelimiterTest {
@@ -52,15 +57,31 @@ public class WhitespaceDelimiterTest {
 	}
 
 	@Test
-	public void reversal1() {
+	public void reversal1() throws FileNotFoundException {
+		String[] rules = { "foo ::= not after <bar> <quux>",//
+				"bar ::= 't' 'c'",//
+				"quux ::= 't' 'd'",//
+		};
+		Grammar g = new Grammar(rules);
+		PrintStream ps = new PrintStream(new FileOutputStream(new File("/tmp/debugging.txt")));
+		Matcher m = g.find("t c t d t d", new Options().log(ps));
+		Match n = m.match();
+		assertNotNull(n);
+		int count = 1;
+		while ((n = m.match()) != null)
+			count++;
+		assertEquals(1, count);
+	}
+
+	@Test
+	public void description1() {
 		String[] rules = { "foo ::= not after <bar> <quux>",//
 				"bar ::= 'the' 'cat'",//
 				"quux ::= 'the' 'dog'",//
 		};
 		Grammar g = new Grammar(rules);
-		Matcher m = g.find("the cat the dog the dog");
-		Match n = m.match();
-		assertNotNull(n);
+		String s = g.describe();
+		assertTrue(s.indexOf(".s") == -1);
 	}
 
 }
